d17b1159420c1ee7e0007e619d64609a65af37fa
abdelrahmanabdelghany <abdelrahman.abdelghany.22@gmail.com>
2023-10-01 02:29:41 +0300

    added train-test transforms

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,26 +2,26 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import os
-from utils.helpers import test_datset_transforms
 
 class Transistor_dataset(Dataset):
     """Transistor_dataset class."""
 
-    def __init__(self, csv_file,test_data=False,transform=None):
+    def __init__(self, csv_file,test_transform=None,train_transform=None):
         """
         Arguments:
             csv_file (string): Path to the csv file with features and labels.
-            test_data (bool, optional): If True, apply test_transform
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+            test_transform (callable, optional): Optional transform to be applied
+                on a sample for test data.
+            train_transform (callable, optional): Optional transform to be applied
+                on a sample for train data.
         
         """
         assert os.path.isfile(csv_file), "File does not exist"
         assert os.path.splitext(csv_file)[1] == ".csv", "File is not a csv file"
         self.Data_Frame = pd.read_csv(csv_file)
         assert len(self.Data_Frame) > 0, "File is empty"
-        self.transform = transform
-        self.test_data = test_data
+        self.train_transform = train_transform
+        self.test_transform = test_transform
 
     def __len__(self):
         return len(self.Data_Frame)
@@ -34,11 +34,12 @@ class Transistor_dataset(Dataset):
         labels = torch.tensor([self.Data_Frame.iloc[idx, -1]])
 
         sample = {'features': features, 'labels': labels}
-        if self.test_data:
-            sample = test_datset_transforms(sample)
+
+        if self.test_transform:
+            sample = self.test_transform(sample)
         
-        if self.transform:
-            sample = self.transform(sample)
+        if self.train_transform:
+            sample = self.train_transform(sample)
 
         return sample
 
